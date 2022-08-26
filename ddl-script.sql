4b5be8b265ed838d0575710fbada995e4c3c4e42
Tiago Rodrigues <tiagords127@gmail.com>
2022-08-26 17:34:07 -0300

    Update ddl-script.sql

--- ddl-script.sql
+++ ddl-script.sql
@@ -4,8 +4,8 @@ use monstrossadb;
 
 create table monstros (
 	cmf varchar(4) primary key,
-    nome varchar(200) not null,
-    salario float not null,
-    cargo varchar(100) not null,
+	nome varchar(200) not null,
+	salario float not null,
+	cargo varchar(100) not null,
     idade int unsigned not null
 );
